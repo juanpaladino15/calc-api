6b013d279f6355707b034abc9803ef5b7becadc7
Guillermo Del Vecchio <guillermo.deve@gmail.com>
2022-04-25 20:59:52 -0300

    feat(controllers): actividad en clase

--- src/controllers.js
+++ src/controllers.js
@@ -25,7 +25,7 @@ router.get("/res/:a/:b", async function (req, res) {
     const params = req.params;
     const a = Number(params.a);
     const b = Number(params.b);
-    const result = a - b;
+    const result = a + b;
 
     await Operation.create({
         type: "RES",
